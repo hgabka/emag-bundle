bbf5783d43696e945d871753b7d9a8496e1dd198
Gabe <hgabka@dev.hgabka.eu>
2017-10-15 10:12:56 +0200

    javitasok

--- src/Repository/EmagPropertyRepository.php
+++ src/Repository/EmagPropertyRepository.php
@@ -3,7 +3,35 @@
 namespace Hgabka\EmagBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Hgabka\EmagBundle\Entity\EmagCategory;
 
 class EmagPropertyRepository extends EntityRepository
 {
+    public function findOneByEmagIdAndCategory($emagId, EmagCategory $category)
+    {
+        return $this
+            ->createQueryBuilder('p')
+            ->where('p.emagId = :emagid')
+            ->andWhere('p.category = :category')
+            ->setParameters([
+                'emagid' => $emagId,
+                'category' => $category,
+            ])
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+
+    public function getPropertiesForCategory(EmagCategory $category)
+    {
+        return $this
+            ->createQueryBuilder('p')
+            ->where('p.category = :category')
+            ->setParameter('category', $category)
+            ->orderBy('p.isRequired', 'DESC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 }
