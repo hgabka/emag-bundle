662849ef9f32381a8c06c9610dff28deb295b7b5
Gabe <hgabka@dev.hgabka.eu>
2017-11-09 15:34:12 +0100

    javitas

--- src/Repository/EmagPropertyRepository.php
+++ src/Repository/EmagPropertyRepository.php
@@ -23,15 +23,36 @@ class EmagPropertyRepository extends EntityRepository
         ;
     }
 
-    public function getPropertiesForCategory(EmagCategory $category)
+    public function getPropertiesForCategoryQb(EmagCategory $category)
     {
         return $this
             ->createQueryBuilder('p')
             ->where('p.category = :category')
             ->setParameter('category', $category)
             ->orderBy('p.isRequired', 'DESC')
+        ;
+    }
+
+    public function getPropertiesForCategory(EmagCategory $category)
+    {
+        return $this
+            ->getPropertiesForCategoryQb($category)
             ->getQuery()
             ->getResult()
         ;
+    }
+
+    public function getPropertyChoicesForCategory(EmagCategory $category)
+    {
+        $props = $this
+            ->getPropertiesForCategory($category)
+        ;
+
+        $results = [];
+        foreach ($props as $prop) {
+            $results[$prop->getId()] = $prop;
+        }
+
+        return $results;
     }
 }
